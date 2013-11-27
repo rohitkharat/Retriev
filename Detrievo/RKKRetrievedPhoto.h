1c31510b20128751f0289951f6a0a4beea89faac
Rohit Kharat <rohit817@gmail.com>
2013-11-26 18:42:06 -0700

    Added class for RetrievedPhotos

---
+++ Detrievo/RKKRetrievedPhoto.h
@@ -0,0 +1,19 @@
+//
+//  RKKRetrievedPhoto.h
+//  Retriev
+//
+//  Created by Rohit Kharat on 11/26/13.
+//  Copyright (c) 2013 rkharat. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreLocation/CoreLocation.h>
+
+@interface RKKRetrievedPhoto : NSObject
+
+@property (nonatomic, strong) NSURL *photoURL;
+@property (nonatomic, strong) NSDate *photoDate;
+@property (nonatomic, strong) CLLocation *photoLocation;
+
+@end
+
---
+++ Detrievo/RKKRetrievedPhoto.m
@@ -0,0 +1,18 @@
+//
+//  RKKRetrievedPhoto.m
+//  Retriev
+//
+//  Created by Rohit Kharat on 11/26/13.
+//  Copyright (c) 2013 rkharat. All rights reserved.
+//
+
+#import "RKKRetrievedPhoto.h"
+
+@implementation RKKRetrievedPhoto
+
+@synthesize photoDate;
+@synthesize photoURL;
+@synthesize photoLocation;
+
+@end
+
