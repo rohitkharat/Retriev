c10cbcfa1b4ccf2ebde8e9ba05cc962304749172
Rohit Kharat <rohit817@gmail.com>
2013-12-09 00:10:25 -0700

    Camera code added

--- Detrievo/RKKCameraViewController.h
+++ Detrievo/RKKCameraViewController.h
@@ -7,7 +7,36 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AddressBook/AddressBook.h>
+#import <AddressBookUI/AddressBookUI.h>
+#import <sqlite3.h>
 
-@interface RKKCameraViewController : UIViewController
+@interface RKKCameraViewController : UIViewController <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+
+{
+    ABPeoplePickerNavigationController *picker ;
+    NSString *databasePath;
+    BOOL taggedMyself;
+}
+
+-(BOOL)createDB;
+-(NSString *)getPhotoLocation;
+
+-(IBAction)showPhotoLibrary:(id)sender;
+
+
+
+@property (nonatomic, retain)  UIImagePickerController *imagePicker;
+@property (nonatomic, retain) NSMutableArray *facesArray;
+@property (nonatomic, retain) UIImage *originalImage;
+@property (nonatomic, retain) CIImage *selectedImage;
+
+@property (nonatomic, strong) NSString *imgURLString;
+
+@property (nonatomic, retain) NSString *city;
+
+@property (nonatomic, retain) NSURL *imgURL;
+
+@property (strong, nonatomic) IBOutlet UIImageView *imageView;
 
 @end
